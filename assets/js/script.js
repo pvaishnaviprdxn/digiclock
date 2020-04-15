cd8a4f959bdec762035840202e4c185939c9ff20
vaishnavi <vaishnavip@prdxn.com>
2020-04-15 19:48:32 +0530

    function added to display append zero to sigle digit time

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,14 @@ function presentTime(){
     mins = updateM(mins);
     secs = updateS(secs);
     document.getElementById("digiclock").innerHTML= hours+" : "+mins+" : "+secs;
+    var time = setTimeout(function(){presentTime()}, 1000);
+}
+//function to display single digit as 2 digits eg: 02;
+function updateT(j) {
+    if (j < 10) {
+        return "0" + j;
+    }
+    else {
+        return j;
+    }
 }
